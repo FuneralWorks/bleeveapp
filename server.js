e4acb6c9be610597b6ecfc24da49075a595861f9
FuneralWorks <nathan.calvarin@gmail.com>
2016-03-05 10:41:14 +0100

    server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+app.use(morgan("dev"));
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.json({limit: '50mb', type : 'application/json'}));
+app.use(bodyParser.urlencoded({limit : '50mb',extended : true}));
+app.use(express.static(__dirname+ "/public"));
+
+require('./app/routes'){app};
+app.listen(port);
+console.log("Server started");
